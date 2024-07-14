2120089c7f4ac6aa9f14172f04082f162dd805c3
Jack <jack@localhost.localdomain>
2024-07-14 18:49:55 -0400

    Arrumando o scrip da fase 2

--- Config_Fase2/config.sh
+++ Config_Fase2/config.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
-sudo mv /etc/rc2.d/S16ssh /etc/
-sudo mv /etc/rc2.d/S91apache2 /etc
+mv /etc/rc2.d/S16ssh /etc/
+mv /etc/rc2.d/S91apache2 /etc
 
-sudo cd /etc/rc2.d/
-sudo rm *
+cd /etc/rc2.d/
+rm *
 
-sudo mv /etc/S16ssh /etc/rc2.d/
+mv /etc/S16ssh /etc/rc2.d/
 
-sudo mv /etc/S91apache2 /etc/rc2.d
+mv /etc/S91apache2 /etc/rc2.d
 
-sudo cd /root
+cd /root
 
 users=(
 
@@ -26,17 +26,18 @@ for user in "${users[@]}"; do
 
     IFS=':' read -r usuario senha comentario <<< "$user"
     
-    sudo useradd -m -c "$comentario" "$usuario"
+    useradd -m -c "$comentario" "$usuario"
 
-    echo "$usuario:$senha" | sudo chpasswd
+    echo "$usuario:$senha" | chpasswd
 
 done 
+
 chat_owner="mariasilva"
 chat_dir="/home/$chat_owner/.chat"
 
-sudo mkdir -p "$chat_dir"
+mkdir -p "$chat_dir"
 
-sudo chown "$chat_owner:$chat_owner" "$chat_dir"
+chown "$chat_owner:$chat_owner" "$chat_dir"
 
 echo "Diretório $chat_dir criado com sucesso."
 
@@ -56,17 +57,18 @@ echo "stou muito bravo com a situação atual no nosso projeto. O gestor da rede
 
 Além disso, o cara não para de brincar com os agendadores de tarefas. Toda vez que eu tento fazer algo, ele já mudou a programação e eu nunca sei o que está realmente acontecendo ou quando as tarefas vão ser executadas. Estou cansado dessa falta de comunicação e dessa bagunça toda. Parece que ele não entende o impacto que essas mudanças constantes têm no nosso trabalho diário. É como se ele estivesse brincando com a nossa paciência e comprometendo a eficiência da equipe inteira.
 
-Não dá mais para aguentar essa situação! Alguém precisa falar para ele que essa atitude está prejudicando todo mundo. Já faz muito tempo que estamos lidando com isso, e é hora de ter uma conversa séria sobre como as coisas estão sendo conduzidas. Precisamos de mais transparência e estabilidade nas nossas rotinas." | sudo tee "$texto1"
-sudo chown "$chat_owner:$chat_owner" "$texto1"
+Não dá mais para aguentar essa situação! Alguém precisa falar para ele que essa atitude está prejudicando todo mundo. Já faz muito tempo que estamos lidando com isso, e é hora de ter uma conversa séria sobre como as coisas estão sendo conduzidas. Precisamos de mais transparência e estabilidade nas nossas rotinas." | tee "$texto1"
+
+chown "$chat_owner:$chat_owner" "$texto1"
 
 
-sudo chmod u+s /usr/bin/nmap
-sudo chmod u+s /usr/bin/python
-sudo chmod u+s /usr/bin/vi
+chmod u+s /usr/bin/nmap
+chmod u+s /usr/bin/python
+chmod u+s /usr/bin/vi
 
-sudo touch /var/test.sh
+touch /var/test.sh
 
-sudo chmod 777 /var/test.sh
+chmod 777 /var/test.sh
 
 
 script_path="/var/test.sh"
@@ -75,16 +77,31 @@ cron_jog="*/5 * * * * /bin/bash $script_path"
 
 echo "$cron_jog" | crontab -
 
+rm -drf /root/*
+
+cp -r Projeto /root
+
+cp /root/Projeto /var/back/
+
+echo "MIAMOTO" > /etc/hostname
+
+sudo usermod -l msfadmin adm
+
+sudo usermod -g adm adm
+
+nova_senha="admin123"
+
+echo "adm:$nova_senha" | sudo chpasswd
+
+sudo usermod -d /home/msfadmin -m adm
 
 
-sudo mkdir projeto 
 
-sudo touch /root/projeto/MOTOCO2.pdf
 
-sudo mkdir -p /var/back/projeto
 
-sudo cp /root/projeto/MOTOCO2.pdf /var/back/projeto
 
-sudo chmod +x /var/back/projeto/MOTOCO2.pdf
+
+
+
 
 

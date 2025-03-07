d13e8a7bf71fdcab8627da1a6f3b08d3dffd7f2b
Denils0n <doc@discente.ifpe.edu.br>
2025-03-07 03:27:20 -0300

    Criando a parte de criptografia das senhas dos usuarios

--- Servidor_web/config.py
+++ Servidor_web/config.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import hashlib
 
+
 from dotenv import load_dotenv
 from faker import Faker
 
@@ -26,13 +27,23 @@ TB_USERNAME = TB_USERNAME_STR.split(",")
 TB_PASSWORD_STR= os.getenv("TB_PASSWORD")
 TB_PASSWORD = TB_PASSWORD_STR.split(",")
 
+# CRIOPGRAFIA DE SENHA
+
+CP_TIPO= os.getenv("CP_TIPO")
+CP_SALT= os.getenv("CP_SALT")
+
+# Arquivo ponto cap 
+
+CAP_ARQUIVO= os.getenv("CAP_ARQUIVO")
+
 
 
 def run_command(command):
-    process = subprocess.run(command, shell=True)
+    process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     if process.returncode != 0:
         print(f"Command {command} failed with code {process.returncode}")
         exit(1)
+    return process.stdout.strip()
 
 #fazendo instalacao do apache2, mysql e php
 run_command('sudo apt update -y ')
@@ -66,19 +77,34 @@ with open('conexao.php', 'w') as f:
 
 #POPULANDO TABLEA USUARIO
 
+
+def criar_hash(senha):
+    
+    if CP_TIPO == 'sha256':
+        command = f"openssl passwd -6 -salt {CP_SALT} {senha}"
+        return run_command(command)
+    
+    md5.update(senha.encode('utf-8'))
+    return md5.hexdigest()
+    
+    
 for i in range(int(TB_QUANTIDADE)):
     if i < len(TB_USERNAME) and i < len(TB_PASSWORD):
-        senha = TB_PASSWORD[i]
-        md5.update(senha.encode('utf-8'))
-        senha = md5.hexdigest()
+        senha = criar_hash(TB_PASSWORD[i])
         run_command(f"mysql -u {DB_USERNAME} -p{DB_PASSWORD} {DB_DATABASE} -e 'INSERT INTO Usuario (nome, senha) VALUES (\"{TB_USERNAME[i]}\", \"{senha}\")'")
     else:
         nome = fake.first_name()
-        senha = fake.password()
-        md5.update(senha.encode('utf-8'))
-        senha = md5.hexdigest()
+        senha = fake.password(length=8, special_chars=False, digits=True, upper_case=False, lower_case=False)
+        senha = criar_hash(senha)  
         run_command(f"mysql -u {DB_USERNAME} -p{DB_PASSWORD} {DB_DATABASE} -e 'INSERT INTO Usuario (nome, senha) VALUES (\"{nome}\", \"{senha}\")'")
 
+
+
+#Copiando aruivo cap
+
+run_command(f"sudo cp {CAP_ARQUIVO} .")
+run_command(f"echo 'Disallow:/{CAP_ARQUIVO}' >> robots.txt")
+run_command('sudo chown -R www-data:www-data /var/www/html/')
 
 #copiando arquivos para o diretorio do apache
 
@@ -91,6 +117,8 @@ run_command('sudo rm /var/www/html/config.py')
 run_command('sudo chown -R www-data:www-data /var/www/html/')
 
 run_command('sudo systemctl restart apache2')
+
+
 
 
 
